37c761b80f382e27db7ccedd7a53b12ec9a219e3
s8122209 <s8122209@live.vu.edu.au>
2025-02-28 10:10:19 +1100

    Update Password Manager.py

--- Password Manager.py
+++ Password Manager.py
@@ -1,8 +1,12 @@
 #Password Manager
+import os
 
 try:
     #create a file to store credentials if it doesnt exist
     f = open("PasswordManager.txt", "x")
+    f.close()
+    f = open("PasswordManager.txt", "w")
+    f.write("Login Credentials:\n")
     f.close()
 except:
     pass
@@ -14,24 +18,19 @@ def addcreds():
     email = input("Enter your email: ")
     password = input("Enter your password: ")
     url = input("Enter your URL/Resource: ")
-    f = open("PasswordManager.txt", "a")
+    f = open("PasswordManager.txt", "a")                                                                                           
     f.write(f"Email: {email} Password: {password} URL/Resource: {url}\n")
     f.close()
     print("Credentials saved.")
     
 def viewcreds():
     #View credentials on file
-    found = False
-    f = open("PasswordManager.txt", "r")
-    user = input("Enter your email: ")
-    for line in f:
-        if user in line:
-            print (f"Credentials found for {user}:\n")
-            print(line)
-            found = True
-    if found != True:
-        print ("Credentials not found.")
-    f.close()    
+    if os.path.getsize("PasswordManager.txt") == 0:
+        print("No credentials saved.")
+    else:
+        f = open("PasswordManager.txt", "r")
+        print(f.read())
+        f.close()    
 
 print("Welcome to the Password Manager!")
 
